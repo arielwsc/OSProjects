f0fba4a1e515ef3a20d130c720716359090551de
Ariel Carvalho <arielwsc17@gmail.com>
2021-03-09 17:03:28 -0600

    Some functionalities of Processor implemented

--- src/Memory.java
+++ src/Memory.java
@@ -4,10 +4,12 @@ public class Memory {
 
     static int[] dataStorage = new int[2000]; //Array of integer for data storage
         //0-999 - User program
+        //999-toward0 - User stack
         //1000-1999 kernel use
+        //1999-toward1000 System stack
 
     public static int read(int address) throws Exception{
-        if (address >= 0 && address <= 1999){
+        if (address >= 0 && address <= dataStorage.length - 1){
             return dataStorage[address];
         }
         else{
@@ -32,8 +34,15 @@ public class Memory {
     public static void main(String args[]) throws Exception{
         String[] arg = new String[3];
         Scanner is = new Scanner(System.in);
+        boolean running = true;
         
-        while(true){
+        while(running){
+            if (is.hasNextBoolean()){
+                if (!is.nextBoolean()){
+                    break;
+                }
+            }
+
             String inputLine = is.nextLine();
             Scanner param = new Scanner(inputLine);
 
@@ -46,7 +55,7 @@ public class Memory {
                 if (arg.length <= 1) {
                     throw new IllegalArgumentException();
                 } else if (arg[0].equals("read")) {
-                    System.out.print(read(Integer.parseInt(arg[1])));
+                    System.out.println(read(Integer.parseInt(arg[1])) + "\n");
                 } else if (arg[0].equals("write")) {
                     write(Integer.parseInt(arg[1]), Integer.parseInt(arg[2]));
                 } else {
@@ -55,7 +64,10 @@ public class Memory {
             }
             catch(Throwable t){
                 System.out.println(t.getMessage()+"\n");
+                System.exit(1);
             }
         }
+        is.close();
+        System.exit(0);
     }   
 }
--- src/Processor.java
+++ src/Processor.java
@@ -1,6 +1,6 @@
 import java.io.*;
+import java.util.Random;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 import java.lang.Runtime;
 
 public class Processor 
@@ -26,35 +26,63 @@ public class Processor
        return proc;
    }
 
-   private static void skipBlankLines(Scanner in){
-      while(in.hasNextLine()){
-         String line = in.nextLine();
-         if(line.length() == 0)
-            continue;
+   public static void loader(PrintWriter pw, InputStream is, String file) throws FileNotFoundException{
+      Scanner sc = new Scanner(is);
+      Scanner inFile = new Scanner(new FileInputStream(file));
+      int index = 0; //Index for both user and kernel mode
+      int userIndex = 0; //Index for only user mode
+      boolean sysMode, userMode; //Flag to indicate for write/read on sys or user mode
+      String pattern = "\\..|\\...|\\....|\\.....";
+
+      while(inFile.hasNext()){
+         if(inFile.hasNext(pattern)){ //Causes the loader to change the load address
+              index = Integer.parseInt(inFile.next().substring(1));
+              inFile.nextLine(); //Ignores rest of line
+
+              if(index >= 0 && index <= 999){ //User mode
+                 userMode = true;
+                 while(userMode){
+                    if(inFile.hasNext(pattern) || !inFile.hasNext()){ //Check for user/kernel switch mode or eof
+                       userMode = false;
+                    }
+                    else{
+                       pw.print("write " + index + " " + inFile.nextInt() + "\n");
+                       pw.flush();
+                       System.out.println(sc.nextLine()); //Prints response from child process
+                       inFile.nextLine();
+                       index++;
+                    }
+                 }
+              }
+
+              if(index >= 1000 && index <= 1999){ //Kernel mode
+                 sysMode = true;
+                 while(sysMode){
+                    if(inFile.hasNext(pattern) || !inFile.hasNext()){ //Check for user/kernel switch mode or eof
+                       sysMode = false;
+                    }
+                    else{
+                       pw.print("write " + index + " " + inFile.nextInt() + "\n");
+                       pw.flush();
+                       System.out.println(sc.nextLine()); //Prints response from child process
+                       inFile.nextLine();
+                       index++;
+                    }
+                 }
+              }
+         }
+         else if (inFile.hasNextInt()){ //Starts to write on user memory
+           pw.print("write " + userIndex + " " + inFile.nextInt() + "\n");
+           pw.flush();
+           System.out.println(sc.nextLine()); //Prints response from child process
+           inFile.nextLine(); //Ignores rest of line
+           userIndex++;
+         }
+         else{
+           inFile.nextLine(); //Ignores rest of line
+         }
       }
    }
-
-   // public static void loader(PrintWriter pw, Scanner sc, String file) throws FileNotFoundException{
-   //    Scanner inFile = new Scanner(new FileInputStream(file));
-   //    int index = 0;
-
-   //    while(inFile.hasNext()){
-   //        if(inFile.next().contains(".")){
-   //             pw.printf("sys.write " + index + " " + inFile.nextInt() + "\n");
-   //             pw.flush();
-   //             System.out.println(sc.nextLine()); //Prints response from child process
-   //             inFile.nextLine(); //Ignores rest of line
-   //             index++;
-   //        }
-   //        else{
-   //          pw.printf("write " + index + " " + inFile.nextInt() + "\n");
-   //          pw.flush();
-   //          System.out.println(sc.nextLine()); //Prints response from child process
-   //          inFile.nextLine(); //Ignores rest of line
-   //          index++;
-   //        }
-   //    }
-   // }
 
    public static void main(String args[]) throws Exception{
 
@@ -70,63 +98,103 @@ public class Processor
             throw new FileNotFoundException("No file to be fed into memory");
          }
 
-         Scanner outputChild = new Scanner(is);
-         Scanner inFile = new Scanner(new FileInputStream(args[0]));
-      
-         int index = 0; //Index for both user and kernel mode
-         int userIndex = 0; //Index for only user mode
-         boolean sysMode, userMode; //Flag to indicate for write/read on sys or user mode
-         String pattern = "\\..|\\...|\\....|\\.....";
+         loader(pw, is, args[0]);
 
-      while(inFile.hasNext()){
-          if(inFile.hasNext(pattern)){ //Causes the loader to change the load address
-               index = Integer.parseInt(inFile.next().substring(1));
-               inFile.nextLine(); //Ignores rest of line
+         int PC, OP; //CPU Registers
+         int SP = 999;
+         int address = 0;
+         int IR = 0;
+         int AC = 0;
+         int X = 0;
+         int Y = 0;
 
-               if(index >= 0 && index <= 999){ //User mode
-                  userMode = true;
-                  while(userMode){
-                     if(inFile.hasNext(pattern) || !inFile.hasNext()){ //Check for user/kernel switch mode or eof
-                        userMode = false;
-                     }
-                     else{
-                        pw.print("write " + index + " " + inFile.nextInt() + "\n");
-                        pw.flush();
-                        System.out.println(outputChild.nextLine()); //Prints response from child process
-                        inFile.nextLine();
-                        index++;
-                     }
-                  }
-               }
+         Scanner sc = new Scanner(is);
+         Random rand = new Random();
 
-               if(index >= 1000 && index <= 1999){ //Kernel mode
-                  sysMode = true;
-                  while(sysMode){
-                     if(inFile.hasNext(pattern) || !inFile.hasNext()){ //Check for user/kernel switch mode or eof
-                        sysMode = false;
-                     }
-                     else{
-                        pw.print("write " + index + " " + inFile.nextInt() + "\n");
-                        pw.flush();
-                        System.out.println(outputChild.nextLine()); //Prints response from child process
-                        inFile.nextLine();
-                        index++;
-                     }
-                  }
-               }
-          }
-          else if (inFile.hasNextInt()){ //Starts to write on user memory
-            pw.print("write " + userIndex + " " + inFile.nextInt() + "\n");
+         while(IR != 50){
+            pw.println("read " + address);
             pw.flush();
-            System.out.println(outputChild.nextLine()); //Prints response from child process
-            inFile.nextLine(); //Ignores rest of line
-            userIndex++;
-          }
-          else{
-            System.out.print(inFile.nextLine()); //Ignores rest of line
-          }
-      }
-         //memory.waitFor();
+            IR = Integer.parseInt(sc.next());
+
+            switch(IR){
+               case 1:
+                  pw.println("read " + (++address));
+                  pw.flush();
+                  AC = Integer.parseInt(sc.next()); //Load the value into the AC
+                  address++;
+                  break;
+               case 2:
+                  AC = address; //Load the value at the address into the AC
+                  address++;
+                  break;
+               case 3:
+                  pw.println("read " + (++address));
+                  pw.flush();
+                  OP = Integer.parseInt(sc.next()); //Load the value from the address found in the given address into the AC
+                  pw.println("read " + OP);
+                  pw.flush();
+                  AC = Integer.parseInt(sc.next());
+                  address++;
+                  break;
+               case 8:
+                  AC = rand.nextInt(101);
+                  address++;
+                  break;
+               case 9:
+                  pw.println("read " + (++address));
+                  pw.flush();
+                  OP = Integer.parseInt(sc.next());
+                  if (OP == 1) System.out.print(AC); //If port=1, writes AC as an int to the screen
+                  if (OP == 2) System.out.print((char)AC); ////If port=2, writes AC as an int to the screen
+                  address++;
+                  break;
+               case 10:
+                  AC += X; //Add the value in X to the AC
+                  address++;
+                  break;
+               case 11:
+                  AC += Y; //Add the value in Y to the AC
+                  address++;
+                  break;
+               case 14:
+                  X = AC; //Copy the value in the AC to X
+                  address++;
+                  break;
+               case 16:
+                  Y = AC; //Copy the value in the AC to Y
+                  address++;
+                  break;
+               case 23:
+                  pw.println("read " + (++address));
+                  pw.flush();
+                  OP = Integer.parseInt(sc.next());
+                  pw.print("write " + SP + " " + (++address) + "\n"); //Push return addr onto stack
+                  pw.flush();
+                  sc.next();
+                  sc.nextLine();
+                  SP--;
+                  address = OP; //Jump to the addr
+                  break;
+               case 24:
+                  if (SP + 1 <= 999){
+                     pw.println("read " + (SP + 1)); //Pop return addr from the stack
+                     pw.flush();
+                     address = Integer.parseInt(sc.next()); //Jump to the addr
+                  }
+                  else{ throw new Exception("Cannot access system memory on user mode");
+                  }
+                  break;
+               case 50:
+                  break; //End of processing
+               default:
+                  throw new Exception("Error fetching to processor");
+            }
+         }
+         pw.println("false");
+         pw.flush();
+         sc.close();
+         memory.waitFor();
+         System.out.println("Memory process ended with code: " + memory.exitValue());
       }
       catch(Throwable t){
          t.printStackTrace();
